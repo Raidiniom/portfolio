13089b1b71f59aa852baab21d25d2d9d13fe8542
raidiniom <raidiniom22ph@gmail.com>
2025-12-25 18:38:59 +0800

    Project Card is done

--- app/projects/data/data.ts
+++ app/projects/data/data.ts
@@ -2,37 +2,37 @@ export const project_details = [
     {
         title: "Test 1",
         description: "This is test 1 description",
-        screenshot: "",
+        screenshot: "./certificate_img/placeholder_cert.jpg",
         link: "",
     },
     {
         title: "Test 2",
         description: "This is test 2 description",
-        screenshot: "",
+        screenshot: "./certificate_img/placeholder_cert.jpg",
         link: "",
     },
     {
         title: "Test 3",
         description: "This is test 3 description",
-        screenshot: "",
+        screenshot: "./certificate_img/placeholder_cert.jpg",
         link: "",
     },
     {
         title: "Test 4",
         description: "This is test 4 description",
-        screenshot: "",
+        screenshot: "./certificate_img/placeholder_cert.jpg",
         link: "",
     },
     {
         title: "Test 5",
         description: "This is test 5 description",
-        screenshot: "",
+        screenshot: "./certificate_img/placeholder_cert.jpg",
         link: "",
     },
     {
         title: "Test 6",
         description: "This is test 6 description",
-        screenshot: "",
+        screenshot: "./certificate_img/placeholder_cert.jpg",
         link: "",
     }
 ]
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,24 +1,19 @@
 'use client';
 
-// TODO: create a card component
-/**
- * parameter:
- * - project object
- * 
- * It must display:
- * - screenshot
- * - name
- * - description
- * - a link to the repository
- */
-
+import Projcard from "../components/Projcard";
 import { project_details } from "./data/data";
 
 export default function Projects() {
     return (
         <>
-            <div>
-                <h2>Projects Page</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
+                {project_details.map((proj, index) => (
+                    <Projcard key={index} 
+                        title={proj.title} 
+                            description={proj.description}
+                            screenshot={proj.screenshot}
+                            link={proj.link} />
+                ))}
             </div>
         </>
     )
